fd35c7a4f280f82b55dfce79ed7966c7562640ac
Geoff Lamperd <glamperd@gmail.com>
2022-06-14 06:02:46 +0000

    KZG

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -9,11 +9,13 @@ fn update_algo() {
 
     let params = Parameters {
         num_g1_elements_needed: 2usize.pow(16),
-        num_g2_elements_needed: 16,
+        num_g2_elements_needed: 2,
     };
 
+    let num_coefficients: usize = 2usize.pow(16);
+
     // Simulate deserialisation
-    let acc = Accumulator::new(params);
+    let acc = Accumulator::new_for_kzg(num_coefficients);
     let bytes = acc.serialise();
     let mut acc = Accumulator::deserialise(&bytes, params, SubgroupCheck::Partial);
 
